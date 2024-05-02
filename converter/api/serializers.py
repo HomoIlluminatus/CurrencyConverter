7c232d59acb236d3858db1fca0fa88854e392ea7
Nikita <nikitaivanov2341@gmail.com>
2024-05-02 21:51:19 +0300

    view and serializer done

--- converter/api/serializers.py
+++ converter/api/serializers.py
@@ -7,3 +7,10 @@ class CurrencyRateSerializer(serializers.ModelSerializer):
     class Meta:
         model = CurrencyRate
         fields = '__all__'
+        
+class CurrencyConversionResponseSerializer(serializers.Serializer):
+    original_amount = serializers.DecimalField(max_digits=10, decimal_places=8)
+    from_currency = serializers.CharField(max_length=3)
+    converted_amount = serializers.DecimalField(max_digits=10, decimal_places=8)
+    to_currency = serializers.CharField(max_length=3)
+    timestamp = serializers.DateTimeField()
--- converter/api/views.py
+++ converter/api/views.py
@@ -1,15 +1,22 @@
-import io
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
+from rest_framework import status
 
 from .models import CurrencyRate
 from .serializers import CurrencyRateSerializer
 
 @api_view(['GET'])
 def get_currency_rate(request):
-    currency_code = request.data[0]['currency_code']
-    instance = CurrencyRate.objects.get(currency_code=currency_code)
+    amount = request.data.get('amount')
+    from_currency = request.data.get('from_currency')
+    to_currency = request.data.get('to_currency')
+    try:
+        exchange_rate_from = CurrencyRate.objects.get(currency_code=from_currency)
+        exchange_rate_to = CurrencyRate.objects.get(currency_code=to_currency)
+    except CurrencyRate.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    
     serializer = CurrencyRateSerializer(instance=instance)
     return Response(serializer.data)
